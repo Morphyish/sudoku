7f3dcd5775e348a1cdcfd9908b8d8691622236b6
Morphyish <thomas.sauques@gmail.com>
2020-10-27 16:08:22 +0100

    :sparkles: History

--- src/stores/history.js
+++ src/stores/history.js
@@ -1,10 +1,62 @@
 import { writable } from 'svelte/store'
 
+const initialState = {
+    entries: [],
+    currentStep: 0,
+}
+
 function historyStore() {
-    const history = writable(null)
+    const history = writable(initialState)
+
+    const addEntry = entry => {
+        history.update(snapshot => ([...snapshot, entry]))
+    }
+
+    const goToStep = step => {
+        history.update(snapshot => {
+            if (step >= 0 && step < snapshot.entries.length) {
+                return {
+                    ...snapshot,
+                    currentStep: step,
+                }
+            }
+
+            return snapshot
+        })
+    }
+
+    const goToNextStep = () => {
+        history.update(snapshot => {
+            const nextStep = snapshot.currentStep + 1
+            if (nextStep < snapshot.entries.length) {
+                return {
+                    ...snapshot,
+                    currentStep: nextStep,
+                }
+            }
+
+            return snapshot
+        })
+    }
+
+    const goToLastStep = () => {
+        history.update(snapshot => ({
+            ...snapshot,
+            currentStep: snapshot.entries.length - 1,
+        }))
+    }
+
+    const reset = () => {
+        history.set(initialState)
+    }
 
     return {
         ...history,
+        addEntry,
+        goToStep,
+        goToNextStep,
+        goToLastStep,
+        reset,
     }
 }
 
