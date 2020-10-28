dcf7030c2e49078e5433508d6722f359134d985d
Morphyish <thomas.sauques@gmail.com>
2020-10-28 23:12:55 +0100

    :fire: Cleaning up Grid store

--- src/stores/grid.js
+++ src/stores/grid.js
@@ -1,15 +1,9 @@
-import { get, writable } from 'svelte/store'
+import { writable } from 'svelte/store'
 import { create, empty, obfuscate } from '../grid'
-import { clone, getCoordinatesFromIndex, removeSymmetricalIndexes } from '../utils'
+import { clone } from '../utils'
 
 function gridStore() {
     const grid = writable(null)
-
-    const getCell = (col, row) => {
-        const snapshot = get(grid)
-
-        return snapshot[row][col]
-    }
 
     const generate = () => {
         const newGrid = create()
@@ -17,28 +11,6 @@ function gridStore() {
         const finishedGrid = empty(obfuscatedGrid)
 
         grid.set(finishedGrid)
-    }
-
-    const trim = () => {
-        grid.update(snapshot => {
-            const gridToTrim = clone(snapshot)
-            const flattenedGrid = gridToTrim.flat()
-
-            const filtered = flattenedGrid.filter(value => value)
-
-            if (filtered.length <= 18) {
-                return gridToTrim
-            }
-
-            let indexToRemove = -1
-
-            while (!flattenedGrid[indexToRemove]) {
-                indexToRemove = Math.floor(Math.random() * 81)
-            }
-
-            const [col, row] = getCoordinatesFromIndex(indexToRemove)
-            return removeSymmetricalIndexes(gridToTrim, col, row)
-        })
     }
 
     const setCell = (col, row, value) => {
@@ -53,9 +25,7 @@ function gridStore() {
     return {
         ...grid,
         generate,
-        getCell,
         setCell,
-        trim,
     }
 }
 
