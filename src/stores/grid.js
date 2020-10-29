d892e26dc471d48b21c8c075a6b255636d34eb2c
Morphyish <thomas.sauques@gmail.com>
2020-10-29 16:43:04 +0100

    :bug: Grid initialState correction

--- src/stores/grid.js
+++ src/stores/grid.js
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store'
 import { create, empty, obfuscate } from '../grid'
 import { clone, setCell as genericSetCell } from '../utils'
 
-const initialState = Array(9).fill([Array(9).fill(0)])
+const initialState = Array(9).fill(Array(9).fill(0))
 
 function gridStore() {
     const grid = writable(initialState)
