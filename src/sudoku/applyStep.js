93d37a5d5b79357bce0273d0142023f5aff70cbd
Morphyish <thomas.sauques@gmail.com>
2020-11-04 12:14:56 +0100

    :art: Apply helpers first

--- src/sudoku/applyStep.js
+++ src/sudoku/applyStep.js
@@ -5,17 +5,17 @@ export function applyStep(grid, helpers, step) {
     let clonedGrid = grid
     let clonedHelpers = helpers
 
+    if (step.helpers) {
+        for (const { col, row, values } of step.helpers) {
+            setCell(clonedHelpers, col, row, values)
+        }
+    }
+
     if (step.grid) {
         for (const { col, row, value } of step.grid) {
             setCell(clonedGrid, col, row, value)
         }
         clonedHelpers = updateHelpers(clonedHelpers, clonedGrid)
-    }
-
-    if (step.helpers) {
-        for (const { col, row, values } of step.helpers) {
-            setCell(clonedHelpers, col, row, values)
-        }
     }
 
     return {
