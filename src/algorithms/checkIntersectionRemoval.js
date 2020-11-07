fcf58ca4c35403e04b15573993bacd07fe9c82ee
Morphyish <thomas.sauques@gmail.com>
2020-11-07 18:58:43 +0100

    :sparkles: Add Intersection Removal algorithm

---
+++ src/algorithms/checkIntersectionRemoval.js
@@ -0,0 +1,175 @@
+import { getCell, getCol, getRow, getSquare } from '../utils'
+
+export function checkIntersectionRemoval(helpers) {
+    for (let i = 0; i < 9; i++) {
+        const [col, row] = getSquareAnchor(i)
+        const squareHelpers = getSquare(helpers, i)
+        const square = [
+            squareHelpers.slice(0, 3),
+            squareHelpers.slice(3, 6),
+            squareHelpers.slice(6, 9),
+        ]
+
+        for (let j = 0; j < 3; j++) {
+            const squareCol = getCol(square, j)
+            const squareRow = getRow(square, j)
+            const squareColHelpers = [...new Set(squareCol.flat())]
+            const squareRowHelpers = [...new Set(squareRow.flat())]
+            // Pointing Tuples
+            const colPointingTuple = pointingTuples(helpers, squareColHelpers, i, col + j, j, 'col')
+            if (colPointingTuple) {
+                return {
+                    helpers: colPointingTuple.steps,
+                }
+            }
+
+            const rowPointingTuple = pointingTuples(helpers, squareColHelpers, i, row + j, j, 'row')
+            if (rowPointingTuple) {
+                return {
+                    helpers: rowPointingTuple.steps,
+                }
+            }
+
+            // Box Line Reduction
+            const boxColReduction = boxLineReduction(helpers, squareColHelpers, col, row, j, 'col')
+            if (boxColReduction) {
+                return {
+                    helpers: boxColReduction.steps,
+                }
+            }
+
+            const boxRowReduction = boxLineReduction(helpers, squareRowHelpers, col, row, j, 'row')
+            if (boxRowReduction) {
+                return {
+                    helpers: boxRowReduction.steps,
+                }
+            }
+        }
+    }
+    return null
+}
+
+function getSquareAnchor(num) {
+    const col = 3 * (num % 3)
+    const row = 3 * Math.floor(num / 3)
+
+    return [col, row]
+}
+
+function pointingTuples(helpers, boxHelpers, num, line, squareLine, zone) {
+    let squareHelpers = getSquare(helpers, num)
+    if (zone === 'col') {
+        // Remove in reverse order to prevent indexes from changing
+        squareHelpers.splice(squareLine + 6, 1)
+        squareHelpers.splice(squareLine + 3, 1)
+        squareHelpers.splice(squareLine, 1)
+    } else {
+        squareHelpers.splice(squareLine * 3, 3)
+    }
+
+    squareHelpers = squareHelpers.flat()
+    const pinnedHelpers = boxHelpers.filter(h => !squareHelpers.includes(h))
+
+    if (pinnedHelpers.length) {
+        // console.log('pinned helpers', pinnedHelpers, 'in', zone, line, 'from square', num, 'squareLine', squareLine)
+        return removeFromLine(helpers, pinnedHelpers, line, num, zone)
+    }
+
+    return null
+}
+
+function removeFromLine(helpers, helpersToRemove, lineIndex, squareToIgnore, zone) {
+    const steps = []
+
+    for (let i = 0; i < 9; i++) {
+        if (i >= squareToIgnore * 3 && i < squareToIgnore * 3 + 3) {
+            continue
+        }
+
+        const col = zone === 'col' ? lineIndex : i
+        const row = zone === 'col' ? i : lineIndex
+
+        helpersToRemove.forEach(valueToRemove => {
+            const cell = getCell(helpers, col, row)
+            const step = removeFromCell(cell, valueToRemove, col, row)
+            if (step) {
+                steps.push(step)
+            }
+        })
+    }
+
+    if (steps.length) {
+        return {
+            steps,
+        }
+    }
+
+    return null
+}
+
+function boxLineReduction(helpers, boxHelpers, col, row, offset, zone) {
+    let lineHelpers = []
+    if (zone === 'col') {
+        lineHelpers = getCol(helpers, col + offset)
+        lineHelpers.splice(row, 3)
+    } else {
+        lineHelpers = getRow(helpers, row + offset)
+        lineHelpers.splice(col, 3)
+    }
+
+    lineHelpers = lineHelpers.flat()
+    const pinnedHelpers = boxHelpers.filter(h => !lineHelpers.includes(h))
+
+    if (pinnedHelpers.length) {
+        // console.log('pinned helpers', pinnedHelpers, 'in square', col, row, zone, offset)
+        const lineToIgnore = zone === 'col' ? col + offset : row + offset
+        return removeFromSquare(helpers, pinnedHelpers, col, row, lineToIgnore, zone)
+    }
+
+    return null
+}
+
+function removeFromSquare(helpers, helpersToRemove, col, row, lineToIgnore, zone) {
+    const steps = []
+
+    for (let i = col; i < col + 3; i++) {
+        if (zone === 'col' && i === lineToIgnore) {
+            continue
+        }
+
+        for (let j = row; j < row + 3; j++) {
+            if (zone === 'row' && j === lineToIgnore) {
+                continue
+            }
+
+            helpersToRemove.forEach(valueToRemove => {
+                const cell = getCell(helpers, i, j)
+                const step = removeFromCell(cell, valueToRemove, i, j)
+                if (step) {
+                    steps.push(step)
+                }
+            })
+        }
+    }
+
+    if (steps.length) {
+        return {
+            steps,
+        }
+    }
+
+    return null
+}
+
+function removeFromCell(cell, valueToRemove, col, row) {
+    if (cell?.includes(valueToRemove)) {
+        const newCell = cell.filter(value => value !== valueToRemove)
+        return {
+            col,
+            row,
+            values: newCell,
+        }
+    }
+
+    return null
+}
---
+++ src/algorithms/checkIntersectionRemoval.spec.js
@@ -0,0 +1,186 @@
+import { initHelpers, updateHelpers } from '../helper'
+import { checkIntersectionRemoval } from './checkIntersectionRemoval'
+
+describe('algorithms/checkIntersectionRemoval', () => {
+    it('should find a pointing tuple in a col', () => {
+        const grid = [
+            [8, 0, 0, 0, 0, 0, 1, 5, 9],
+            [2, 1, 0, 5, 0, 8, 0, 0, 4],
+            [5, 6, 0, 0, 0, 0, 0, 0, 8],
+            [0, 5, 0, 9, 3, 0, 0, 0, 0],
+            [6, 3, 0, 2, 0, 0, 0, 9, 5],
+            [0, 0, 2, 8, 5, 6, 0, 0, 0],
+            [1, 8, 0, 0, 0, 3, 5, 7, 0],
+            [0, 2, 0, 0, 0, 5, 9, 8, 0],
+            [0, 0, 5, 0, 8, 0, 4, 0, 0],
+        ]
+
+        const helpers = updateHelpers(initHelpers(), grid)
+        const result = checkIntersectionRemoval(helpers)
+
+        // console.log(helpers)
+
+        expect(result).not.toBeNull()
+        expect(result.helpers).toEqual([
+            {
+                col: 2,
+                row: 6,
+                values: [4, 6],
+            },
+            {
+                col: 2,
+                row: 7,
+                values: [4, 6, 7],
+            },
+        ])
+    })
+
+    it('should find a pointing tuple in a row', () => {
+        const grid = [
+            [0, 1, 7, 9, 0, 3, 6, 0, 0],
+            [0, 0, 0, 0, 8, 0, 0, 0, 0],
+            [9, 0, 0, 0, 0, 0, 5, 0, 7],
+            [0, 7, 2, 0, 1, 0, 4, 3, 0],
+            [0, 0, 0, 4, 0, 2, 0, 7, 0],
+            [0, 6, 4, 3, 7, 0, 2, 5, 0],
+            [7, 0, 1, 0, 0, 0, 0, 6, 5],
+            [0, 0, 0, 0, 3, 0, 0, 0, 0],
+            [0, 0, 5, 6, 0, 1, 7, 2, 0],
+        ]
+
+        const helpers = updateHelpers(initHelpers(), grid)
+        const result = checkIntersectionRemoval(helpers)
+
+        expect(result).not.toBeNull()
+        expect(result.helpers).toEqual([
+            {
+                col: 0,
+                row: 1,
+                values: [2, 4, 5, 6],
+            },
+            {
+                col: 1,
+                row: 1,
+                values: [2, 4, 5],
+            },
+            {
+                col: 2,
+                row: 1,
+                values: [6],
+            },
+        ])
+    })
+
+    it('should find a box line reduction in a col', () => {
+        const grid = [
+            [8, 0, 3, 0, 0, 7, 0, 1, 6],
+            [0, 0, 0, 8, 0, 0, 0, 9, 0],
+            [0, 6, 0, 0, 0, 1, 8, 7, 0],
+            [3, 0, 0, 0, 0, 0, 0, 4, 8],
+            [0, 8, 2, 0, 0, 9, 6, 5, 0],
+            [6, 5, 0, 0, 0, 0, 0, 3, 9],
+            [0, 2, 0, 9, 0, 0, 0, 6, 0],
+            [9, 3, 6, 0, 0, 2, 0, 8, 0],
+            [5, 1, 0, 6, 0, 0, 9, 2, 4],
+        ]
+
+        const helpers = updateHelpers(initHelpers(), grid)
+        const result = checkIntersectionRemoval(helpers)
+
+        expect(result).not.toBeNull()
+        expect(result.helpers).toEqual([
+            {
+                col: 0,
+                row: 1,
+                values: [1, 2, 7],
+            },
+            {
+                col: 0,
+                row: 2,
+                values: [2],
+            },
+            {
+                col: 2,
+                row: 1,
+                values: [1, 5, 7],
+            },
+            {
+                col: 2,
+                row: 2,
+                values: [5, 9],
+            },
+        ])
+    })
+
+    it('should find a box line reduction in a row', () => {
+        const grid = [
+            [0, 0, 7, 0, 1, 6, 8, 0, 3],
+            [8, 0, 0, 0, 9, 0, 0, 0, 0],
+            [0, 0, 1, 8, 7, 0, 0, 6, 0],
+            [0, 0, 0, 2, 4, 8, 3, 0, 0],
+            [0, 0, 9, 6, 5, 0, 0, 8, 2],
+            [0, 0, 0, 0, 3, 9, 6, 5, 0],
+            [9, 0, 0, 0, 6, 0, 0, 2, 0],
+            [0, 0, 2, 0, 8, 0, 9, 3, 6],
+            [6, 0, 0, 9, 2, 4, 5, 1, 0],
+        ]
+
+        const helpers = updateHelpers(initHelpers(), grid)
+        const result = checkIntersectionRemoval(helpers)
+
+        expect(result).not.toBeNull()
+        expect(result.helpers).toEqual([
+            {
+                col: 0,
+                row: 2,
+                values: [3, 4, 5],
+            },
+            {
+                col: 1,
+                row: 1,
+                values: [3, 4, 5, 6],
+            },
+            {
+                col: 1,
+                row: 2,
+                values: [3, 4, 5, 9],
+            },
+        ])
+    })
+
+    it('should return null if it can\'t find a solution', () => {
+        const helpers = [
+            [[1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2]],
+            [[1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2]],
+            [[1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2]],
+            [[1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2]],
+            [[1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2]],
+            [[1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2]],
+            [[1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2]],
+            [[1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2]],
+            [[1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2], [1, 2]],
+        ]
+
+        const result = checkIntersectionRemoval(helpers)
+
+        expect(result).toBeNull()
+    })
+
+    it('should return null if helpers are empty', () => {
+        const helpers = [
+            [[], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], []],
+            [[], [], [], [], [], [], [], [], []],
+        ]
+
+        const result = checkIntersectionRemoval(helpers)
+
+        expect(result).toBeNull()
+    })
+})
