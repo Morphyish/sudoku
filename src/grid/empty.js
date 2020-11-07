43cbed50a8a469f183a52b8992128025db8f7709
Morphyish <thomas.sauques@gmail.com>
2020-11-07 19:37:26 +0100

    :sparkles: Try to remove additional cells after all pairs

--- src/grid/empty.js
+++ src/grid/empty.js
@@ -5,13 +5,29 @@ export function empty(grid) {
     const cellIndexes = Array.from(Array(81).keys())
     const shuffledCellIndexes = shuffle(cellIndexes)
 
-    return removeNextPair(grid, shuffledCellIndexes)
+    const {grid: emptiedGrid, methods, nbOfCells, leftoverIndexes } = removeNextPair(grid, shuffledCellIndexes)
+    if (nbOfCells > 17) {
+        const shuffledLeftovers = shuffle(leftoverIndexes)
+        const { grid: finalGrid, methods: finalMethods, nbOfCells: finalNbOfCells } = removeNextCell(emptiedGrid, shuffledLeftovers)
+        return {
+            grid: finalGrid,
+            methods: finalMethods || methods,
+            nbOfCells: finalNbOfCells || nbOfCells,
+        }
+    }
+
+    return {
+        grid: emptiedGrid,
+        methods,
+        nbOfCells,
+    }
 }
 
 function removeNextPair(grid, cellIndexes) {
     if (cellIndexes.length < 18) {
         return {
             grid,
+            leftoverIndexes: cellIndexes,
         }
     }
 
@@ -36,7 +52,40 @@ function removeNextPair(grid, cellIndexes) {
             cellIndexes.splice(index, 1)
             const symIndex = cellIndexes.indexOf(symCellIndex)
             cellIndexes.splice(symIndex, 1)
-            const { grid: updatedGrid, methods: nextMethods } = removeNextPair(clonedGrid, cellIndexes)
+            const { grid: updatedGrid, methods: nextMethods, leftoverIndexes } = removeNextPair(clonedGrid, cellIndexes)
+            return {
+                grid: updatedGrid,
+                methods: nextMethods || methods,
+                nbOfCells: cellIndexes.length,
+                leftoverIndexes,
+            }
+        }
+    }
+
+    return {
+        grid,
+        leftoverIndexes: cellIndexes,
+    }
+}
+
+function removeNextCell(grid, cellIndexes) {
+    if (cellIndexes.length < 18) {
+        return {
+            grid,
+        }
+    }
+
+    for (const cellIndex of cellIndexes) {
+        const clonedGrid = clone(grid)
+
+        const [col, row] = getCoordinatesFromIndex(cellIndex)
+        setCell(clonedGrid, col, row, 0)
+
+        const { isSolvable, methods } = solve(clonedGrid)
+        if (isSolvable) {
+            const index = cellIndexes.indexOf(cellIndex)
+            cellIndexes.splice(index, 1)
+            const { grid: updatedGrid, methods: nextMethods } = removeNextCell(clonedGrid, cellIndexes)
             return {
                 grid: updatedGrid,
                 methods: nextMethods || methods,
