1790a132ef621281977cdf7db226b97e7e6882d3
Morphyish <thomas.sauques@gmail.com>
2020-11-09 11:33:32 +0100

    :bug: Fix a bug for square updates

--- src/algorithms/sectorUpdate.js
+++ src/algorithms/sectorUpdate.js
@@ -7,10 +7,11 @@ export function sectorUpdate(helpers) {
         const squares = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
         getRow(helpers, i).forEach(fill(rows))
-        const rowIdx = rows.indexOf(1)
-        if (rowIdx) {
+        const rowValueIdx = rows.indexOf(1)
+        if (rowValueIdx) {
+            const value = rowValueIdx + 1
             for (let j = 0; j < 9; j++) {
-                const update = checkHelper(helpers, j, i, rowIdx + 1)
+                const update = checkHelper(helpers, j, i, value)
                 if (update) {
                     return {
                         grid: update
@@ -20,10 +21,11 @@ export function sectorUpdate(helpers) {
         }
 
         getCol(helpers, i).forEach(fill(cols))
-        const colIdx = cols.indexOf(1)
-        if (colIdx) {
+        const colValueIdx = cols.indexOf(1)
+        if (colValueIdx) {
+            const value = colValueIdx + 1
             for (let j = 0; j < 9; j++) {
-                const update = checkHelper(helpers, i, j, colIdx + 1)
+                const update = checkHelper(helpers, i, j, value)
                 if (update) {
                     return {
                         grid: update
@@ -33,18 +35,19 @@ export function sectorUpdate(helpers) {
         }
 
         getSquare(helpers, i).forEach(fill(squares))
-        const squareIdx = squares.indexOf(1)
-        if (squareIdx) {
-            const rowsAnchor = 3 * Math.floor(i / 3)
+        const squareValueIdx = squares.indexOf(1)
+        if (squareValueIdx) {
+            const value = squareValueIdx + 1
             const colsAnchor = 3 * (i % 3)
+            const rowsAnchor = 3 * Math.floor(i / 3)
             for (let j = 0; j < 3; j++) {
                 for (let k = 0; k < 3; k++) {
-                    const [col, row] = [j + rowsAnchor, k + colsAnchor]
+                    const [col, row] = [j + colsAnchor, k + rowsAnchor]
                     const update = checkHelper(
                         helpers,
                         col,
                         row,
-                        squareIdx + 1,
+                        value,
                     )
                     if (update) {
                         return {
