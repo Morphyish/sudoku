49b8592b423279b0e22dda8b2aa63d3c9079607d
Morphyish <thomas.sauques@gmail.com>
2020-11-09 12:18:26 +0100

    :white_check_mark: rewrite tests to use grids

--- src/algorithms/checkHiddenPair.spec.js
+++ src/algorithms/checkHiddenPair.spec.js
@@ -1,3 +1,4 @@
+import { getHelpers } from '../helper'
 import { checkHiddenPair } from './checkHiddenPair'
 
 describe('algorithms/checkHiddenPair', () => {
@@ -127,19 +128,20 @@ describe('algorithms/checkHiddenPair', () => {
         expect(result).toBeNull()
     })
 
-    it('should return null if helpers are empty', () => {
-        const helpers = [
-            [[], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], []],
-            [[], [], [], [], [], [], [], [], []],
+    it('should return null if the grid is full', () => {
+        const grid = [
+            [8, 4, 2, 5, 6, 1, 3, 9, 7],
+            [7, 1, 9, 8, 4, 3, 2, 6, 5],
+            [3, 6, 5, 9, 7, 2, 4, 8, 1],
+            [4, 2, 8, 1, 3, 7, 9, 5, 6],
+            [1, 9, 3, 6, 5, 4, 7, 2, 8],
+            [5, 7, 6, 2, 9, 8, 1, 4, 3],
+            [9, 8, 1, 3, 2, 5, 6, 7, 4],
+            [6, 5, 4, 7, 1, 9, 8, 3, 2],
+            [2, 3, 7, 4, 8, 6, 5, 1, 9],
         ]
 
+        const helpers = getHelpers(grid)
         const result = checkHiddenPair(helpers)
 
         expect(result).toBeNull()
